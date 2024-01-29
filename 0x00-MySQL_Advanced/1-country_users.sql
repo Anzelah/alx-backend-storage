4c6e1f5d5b321c2693d61b1f713038649a67be06
Anzelah <jepkoechanzellah@gmail.com>
2024-01-29 17:32:48 +0000

    use create table and not alter

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,4 +1,7 @@
 -- create users for our table
-ALTER TABLE users ADD (
-	country ENUM('US', 'CO', 'TN') NOT NULL
+CREATE TABLE IF NOT EXISTS users (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        name VARCHAR(255),
+        country ENUM('US', 'CO', 'TN') NOT NULL
 );
