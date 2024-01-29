9534514f221e005da8e7c1f345c3fb4be1363544
Anzelah <jepkoechanzellah@gmail.com>
2024-01-29 22:00:05 +0000

    first trial

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Write a SQL script that lists all bands with Glam rock
 -- as their main style, ranked by their longevity
-SELECT band_name, DATEDIFF(split, formed) AS lifespan
+SELECT band_name, DATEDIFF(formed, split) AS lifespan
 WHERE style = "Glam rock"
 ORDER BY lifespan DESC;
