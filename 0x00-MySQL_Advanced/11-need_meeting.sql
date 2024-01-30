f58b547f20c5093212c3c9512648fa3e1efe8724
Anzelah <jepkoechanzellah@gmail.com>
2024-01-30 19:22:22 +0000

    use datediff() to find time since last meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,5 +2,4 @@
 CREATE VIEW need_meeting AS
 SELECT * FROM students
 WHERE (score < 80) AND
-(TIMESTAMPDIFF((MONTH, last_meeting, CURDATE()) > 1 )
-OR (ISNULL(last_meeting) = 1));
+(FLOOR (DATEDIFF(CURRENT_DATE, last_meeting) > 30) OR (ISNULL(last_meeting) = 1));
