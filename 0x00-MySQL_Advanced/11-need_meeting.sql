81612304da601b7194ba3f8cefb8236c94edf61f
Anzelah <jepkoechanzellah@gmail.com>
2024-01-30 19:34:13 +0000

    only need to see name from column

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,5 +1,5 @@
 -- create a view
 CREATE VIEW need_meeting AS
-SELECT * FROM students
+SELECT name FROM students
 WHERE (score < 80) AND
 (FLOOR (DATEDIFF(CURRENT_DATE, last_meeting) > 30) OR (ISNULL(last_meeting) = 1));
