f59e55de2d4e35ec6711e4dba02458d4f4949606
Anzelah <jepkoechanzellah@gmail.com>
2024-01-30 21:51:59 +0000

    specify delimitr

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,8 +1,12 @@
 -- create a trigger
+DELIMITER $$
+
 CREATE TRIGGER my_trigger AFTER
 INSERT ON orders FOR EACH ROW
 BEGIN
 UPDATE items 
-SET quantity = quantity - NEW.quantity
-WHERE items.number = NEW.number;
+SET quantity = quantity - 1
+WHERE name = NEW.item_name;
 END$$
+
+DELIMITER ;
