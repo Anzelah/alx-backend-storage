d9009fd35afbc612bc919dc65bf5f0ac94314d9c
Anzelah <jepkoechanzellah@gmail.com>
2024-01-30 19:37:06 +0000

    add descriptive comment

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,4 +1,5 @@
--- create a view
+-- create a view that lists all students that have a score under 80
+-- and no last_meeting or more than 1 month.
 CREATE VIEW need_meeting AS
 SELECT name FROM students
 WHERE (score < 80) AND
