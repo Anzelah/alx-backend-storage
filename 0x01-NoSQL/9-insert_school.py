346f67bdaea76ef53ac048586fe0eb085a7fe1ba
Anzelah <jepkoechanzellah@gmail.com>
2024-02-04 06:33:56 -0800

    use inserted_id to retrieve the new id

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,4 +4,4 @@
 def insert_school(mongo_collection, **kwargs):
     """insert a new document in a collection based on kwargs"""
     res = mongo_collection.insert_one(kwargs)
-    return res
+    return res.inserted_id
