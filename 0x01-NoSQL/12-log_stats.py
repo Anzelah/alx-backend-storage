25cedfa77285b00b4c24c582f796c08af06a2689
Anzelah <jepkoechanzellah@gmail.com>
2024-02-04 10:33:54 -0800

    first trial

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Import your modules"""
+
+from pymongo import MongoClient
+
+
+client = MongoClient()
+
+db = client.logs
+col = db.nginx
+
+all_docs = col.count_documents({})
+
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+method_docs = {col.count_documents({"method": m} for m in methods)
+
+match_docs = col.count_documents({"method": "GET"}, {"path": "/status"})
+
+print(f"{all_docs} logs")
+print("Methods:")
+for met in methods:
+    print(f"\tmethod {met}: {method_docs[met]}")
+print(f"{match_docs} status check")
