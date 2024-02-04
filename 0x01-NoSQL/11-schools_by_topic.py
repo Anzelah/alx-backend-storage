604bdf665430a8a018bc9a4b6d2f749994393e4a
Anzelah <jepkoechanzellah@gmail.com>
2024-02-04 08:30:50 -0800

    first trial

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Import your modules"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Return list of schools having a specific topic"""
+    res = mongo_collection.find({"topic": topic})
+    return res
