f89ad32424b134396f4b5ce097156a6054ef5ecc
Anzelah <jepkoechanzellah@gmail.com>
2024-02-04 06:31:56 -0800

    insert a document int a collection

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Import your modules"""
+
+def insert_school(mongo_collection, **kwargs):
+    """insert a new document in a collection based on kwargs"""
+    res = mongo_collection.insert_one(kwargs)
+    return res
