b2d4bdfb9f03515966de025bdb1b9f09e90ea3dd
Anzelah <jepkoechanzellah@gmail.com>
2024-02-05 14:01:48 +0000

    annotate data crrectly

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,7 @@
 
 import redis
 from uuid import uuid4
+from typing import Union
 
 
 class Cache:
@@ -12,7 +13,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: any) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store your data into redis"""
         random_key = str(uuid4())
         self._redis.set(random_key, data)
