d8e50c581754aaf04464f9c1947ba846794f912f
Anzelah <jepkoechanzellah@gmail.com>
2024-02-05 15:15:01 +0000

    syntax error

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,7 +19,7 @@ class Cache:
         self._redis.set(random_key, data)
         return random_key
 
-    def get(self.key: str, fn=None) -> any:
+    def get(self, key: str, fn=None) -> any:
         """Get your data from redis db"""
         if not self._redis.exists(key):
             return None 
