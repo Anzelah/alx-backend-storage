5411b1e8680c3d971c34ea255ff560cf276f58eb
Anzelah <jepkoechanzellah@gmail.com>
2024-02-05 20:34:39 +0000

    fix inde out or range error

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,6 +18,7 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwds)
     return wrapper
 
+
 def call_history(method: Callable) -> Callable:
     """Decorator to store the history of inputs and
     outputs for a particular function"""
@@ -26,12 +27,12 @@ def call_history(method: Callable) -> Callable:
         """Wrapper function to implement counting logic"""
         input_key = ("{}:inputs" .format(method.__qualname__))
         output_key = ("{}:outputs" .format(method.__qualname__))
-        
+
         self._redis.rpush(input_key, str(args))
         output = method(self, *args, **kwds)
         self._redis.rpush(output_key, str(output))
         return output
-    return wrapper 
+    return wrapper
 
 
 class Cache:
@@ -48,7 +49,7 @@ class Cache:
         random_key = str(uuid4())
         self._redis.set(random_key, data)
         return random_key
-    
+
     def get(self, key: str, fn=None) -> any:
         """Get your data from redis db"""
         val = self._redis.get(key)
@@ -60,24 +61,24 @@ class Cache:
         """Use correct conversion function depending on value returned"""
         val = self._redis.get(key)
         return val.decode("utf-8")
-    
+
     def get_int(self, key: str) -> int:
         """Use correct conversion function depending on value returned"""
         val = self._redis.get(key)
         return int(val.decode("utf-8"))
-    
+
 
 def replay(method: Callable):
     """Display the history of calls of a particular function."""
     met = method.__qualname__
     input_key = ("{}:inputs" .format(met))
     output_key = ("{}:outputs" .format(met))
-    
+
     slf = method.__self__._redis
     count = slf.get(met).decode("utf=8")
     print("{} was called {} times" .format(met, count))
 
     inputs = slf.lrange(input_key, 0, -1)
     outputs = slf.lrange(output_key, 0, -1)
-    for i, o in zip(inputs, outputs):
+    for i, o in list(zip(inputs, outputs)):
         ("{}(*{}) -> {}" .format(met, i, o))
