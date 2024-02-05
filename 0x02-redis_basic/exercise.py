5731bb03145ad8e70fd9bd4135612613da1f2d49
Anzelah <jepkoechanzellah@gmail.com>
2024-02-05 14:38:53 +0000

    first trial

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,3 +18,11 @@ class Cache:
         random_key = str(uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(key: str, fn=None):
+        """Get your data from redis db"""
+        if not self._redis.exists(key):
+            return None
+        
+        return self._redis.get(key).fn()
+
